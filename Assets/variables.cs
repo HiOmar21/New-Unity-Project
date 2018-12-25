7fe3d403b24a5dd2aa94aa8b219f03e6a7059bf6
HiOmar21 <omar.ramos231@hotmail.com>
2018-12-25 16:06:48 -0600

    segundo commit

---
+++ Assets/variables.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour
+{
+    string saludo = "Hola soy Omar";
+    void Start()
+    {
+        Debug.Log(saludo);
+    }
+}
+
+
